2d4fe23accd63108d6b1942d73fa1f359aba820f
NakulChauhan1 <48352040+NakulChauhan1@users.noreply.github.com>
2020-11-23 15:38:33 +0530

    added LOW POWER MODES via SLEEPONEXIT feature Application

---
+++ LOW_POWER_MODE_using_WFE/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Inc/stm32f4xx_hal_conf.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Inc/main.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cm4.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmInstr.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/cmsis_gcc.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmFunc.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmSimd.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Inc/stm32f4xx_hal_conf.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Inc/main.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cm4.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmInstr.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmFunc.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Include/core_cmSimd.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+N:/Workspace/MCU2_Udemy_Course/My_Workspace/MasteringMCU2_WS/TIMER_Time_base_100ms_IT/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
